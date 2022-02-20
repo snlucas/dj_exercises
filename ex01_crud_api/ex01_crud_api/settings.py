f19f76c2cebb5db0823657ade51c63112ed189b0
Lucas S. Nogueira <lucas.lambda.101@gmail.com>
2022-02-20 15:14:34 -0300

    Finish exercise

--- ex01_crud_api/ex01_crud_api/settings.py
+++ ex01_crud_api/ex01_crud_api/settings.py
@@ -17,7 +17,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "books",
+    "crud",
+    # Third Party
+    "rest_framework",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
